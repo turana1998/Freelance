228b96f5d24f7b5303b9d67105aca6c5c709cc0a
turana1998 <turana221298@gmail.com>
2022-01-17 20:57:01 +0300

    add

--- cvgenerator/app.js
+++ cvgenerator/app.js
@@ -26,12 +26,77 @@ TextEditor(inputfields["academics"]).then(nEditor => {
 
 
 function toggle() {
+  event.preventDefault()
   if (inputShow) {
     inputfields.style.display = "none"
     inputShow = false
     output.innerHTML = `
          
        <div id="invoice">
+       <div class="">
+        <div class="container d-flex">
+            <div class="w-30">
+                <div class="left">
+                    <img  src="./user.png" />
+                    <h2>Elaqe Melumatlari</h2>
+                    <ul>
+                        <li>
+                            <i class="bi bi-pin-map"></i>
+                            <a>${inputfields["contact"].value}
+                            </a>
+                        </li>
+                        <li>
+                            <i class="bi bi-envelope"></i>
+                            <a>hello@reallygreatsite.com
+                            </a>
+                        </li>
+                    </ul>
+                    <h2>Bacariqlar</h2>
+                    <p${inputfields["skills"].value}</p>
+                </div>
+            </div>
+            <div class="w-70">
+                <div class="right">
+                    <h1>${inputfields["name"].value}</h1>
+                    <hr>
+                    <p class="profession">${inputfields["title"].value}</p>
+                    <div class="">
+                        <h3>P E R S O N A L  P R O F I L E</h3>
+                        <p>${inputfields["objective"].value}</p>
+                        <h3>W O R K E X P E R I E N C E</h3>
+                        <p>
+                        ${workExpdetails.getData()}
+                        </p>
+                        <h3>Tehsil</h3>
+                        <p>
+                        ${Academic.getData()}
+                        </p>
+                        <h3>Personal</h3>
+                        <p>
+                        ${inputfields["projects"].value}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+          
+  
+         `
+  } else {
+    inputfields.style.display = "block"
+    inputShow = true
+    output.innerHTML = ""
+  }
+}
+function toggle2() {
+  event.preventDefault()
+  if (inputShow) {
+    inputfields.style.display = "none"
+    inputShow = false
+    output.innerHTML = `
+         
+       <div id="invoice2">
        <div class="">
         <div class="container d-flex">
             <div class="w-30">
--- cvgenerator/pdf.js
+++ cvgenerator/pdf.js
@@ -2,6 +2,7 @@ window.onload = function () {
     document.getElementById("download")
         .addEventListener("click", () => {
             const invoice = document.getElementById("invoice");
+            const invoice2 = document.getElementById("invoice2");
             console.log(invoice);
             console.log(window);
             var opt = {
@@ -12,5 +13,6 @@ window.onload = function () {
                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
             };
             html2pdf().from(invoice).set(opt).save();
+            html2pdf().from(invoice2).set(opt).save();
         })
 }
